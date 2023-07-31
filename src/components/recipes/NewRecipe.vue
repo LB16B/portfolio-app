7e6e14f8905125e9e1e8a4507029bda4af65566f
LB16B <ogawa.yui2000@gmail.com>
2023-08-01 08:05:29 +0900

    create

--- src/components/recipes/NewRecipe.vue
+++ src/components/recipes/NewRecipe.vue
@@ -1,15 +1,49 @@
 <template>
     <div class="relative">
-        <input type="text"
+        <span>Title</span>
+        <input
             class="form-control form-control-lg padding-right-lg"
             placeholder="+ Add new recipe. Press enter to save."
-            @keydown.enter="addNewRecipe"
+            v-model="inputtingTitle" type="text"
             />
-    </div>
+            <!-- @keydown.enter="addNewRecipe" -->
+        </div>
+    <div class="relative">
+        <span>Time</span>
+        <input
+            class="form-control form-control-lg padding-right-lg"
+            placeholder="+ Add new recipe. Press enter to save."
+            v-model="inputtingTime" type="number"
+            />
+            <!-- @keydown.enter="addNewRecipe" -->
+        </div>
+    <div class="relative">
+        <span>Price</span>
+        <input
+            class="form-control form-control-lg padding-right-lg"
+            placeholder="+ Add new recipe. Press enter to save."
+            v-model="inputtingPrice" type="number"
+            />
+            <!-- @keydown.enter="addNewRecipe" -->
+        </div>
+    <div class="relative">
+        <span>Filename</span>
+        <input
+            class="form-control form-control-lg padding-right-lg"
+            placeholder="+ Add new recipe. Press enter to save."
+            v-model="inputtingFilename" type="text"
+            />
+            <!-- @keydown.enter="addNewRecipe" -->
+        </div>
+        <button @click="addNewRecipe">create</button>
 </template>
 
 <script setup>
-import { reactive } from 'vue'
+import { ref, reactive, defineEmits } from 'vue'
+const inputtingTitle = ref('')
+const inputtingTime = ref('')
+const inputtingPrice = ref('')
+const inputtingFilename = ref('')
 
 const newRecipe = reactive({
     title: '',
@@ -21,17 +55,15 @@ const newRecipe = reactive({
 const emit = defineEmits(['added'])
 
 const addNewRecipe = event => {
-    if (event.target.value.trim()) {
-        newRecipe.title = event.target.value
-        newRecipe.time = 4;
-        newRecipe.price = '4';
-        newRecipe.filename = 'ddddd';
-        // newRecipe.title = event.target.value
-        // newRecipe.time = '';
-        // newRecipe.price = '';
-        // newRecipe.filename = '';
+    // if (event.target.value.trim()) {
+        newRecipe.title = inputtingTitle.value
+        newRecipe.time = inputtingTime.value
+        newRecipe.price = inputtingPrice
+        newRecipe.filename = inputtingFilename
         event.target.value = ""
+        console.log(inputtingTitle)
         emit('added', newRecipe)
-    }
+    // }
 }
+
 </script>

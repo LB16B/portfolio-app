8cd054745c49965abc13bf87a971264b25489e67
LB16B <ogawa.yui2000@gmail.com>
2023-08-01 21:51:56 +0900

    image

--- src/components/recipes/NewRecipe.vue
+++ src/components/recipes/NewRecipe.vue
@@ -7,7 +7,7 @@
             v-model="inputtingTitle" type="text"
             />
             <!-- @keydown.enter="addNewRecipe" -->
-        </div>
+    </div>
     <div class="relative">
         <span>Time</span>
         <input
@@ -16,7 +16,7 @@
             v-model="inputtingTime" type="number"
             />
             <!-- @keydown.enter="addNewRecipe" -->
-        </div>
+    </div>
     <div class="relative">
         <span>Price</span>
         <input
@@ -25,17 +25,17 @@
             v-model="inputtingPrice" type="number"
             />
             <!-- @keydown.enter="addNewRecipe" -->
-        </div>
-    <div class="relative">
+    </div>
+    <!-- <div class="relative">
         <span>Filename</span>
         <input
             class="form-control form-control-lg padding-right-lg"
             placeholder="+ Add new recipe. Press enter to save."
             v-model="inputtingFilename" type="text"
             />
-            <!-- @keydown.enter="addNewRecipe" -->
-        </div>
-        <button @click="addNewRecipe">create</button>
+    </div> -->
+    <input type="file" @change="onFileChange" />
+    <button @click="addNewRecipe">create</button>
 </template>
 
 <script setup>
@@ -44,6 +44,7 @@ const inputtingTitle = ref('')
 const inputtingTime = ref('')
 const inputtingPrice = ref('')
 const inputtingFilename = ref('')
+const selectedFile = ref(null);
 
 const newRecipe = reactive({
     title: '',
@@ -52,16 +53,28 @@ const newRecipe = reactive({
     filename: ''
 })
 
+// ファイルが選択された際に呼ばれる処理
+const onFileChange = event => {
+    const file = event.target.files[0];
+    if (file) {
+    // 選択されたファイルをリアクティブな変数にセット
+    selectedFile.value = file;
+    }
+};
+
 const emit = defineEmits(['added'])
 
 const addNewRecipe = event => {
+    const currentDate = new Date();
+    const formattedDate = currentDate.toISOString().slice(0, 10);
+    const formattedTime = currentDate.toLocaleTimeString().replace(/:/g, '-');
+
     // if (event.target.value.trim()) {
         newRecipe.title = inputtingTitle.value
         newRecipe.time = inputtingTime.value
-        newRecipe.price = inputtingPrice
-        newRecipe.filename = inputtingFilename
+        newRecipe.price = inputtingPrice.value
+        newRecipe.filename =  `${formattedDate}_${formattedTime}_${selectedFile.value.name}`;
         event.target.value = ""
-        console.log(inputtingTitle)
         emit('added', newRecipe)
     // }
 }

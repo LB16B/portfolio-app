3fddd628a7ec58d0fdaaaa6765519af15661583b
LB16B <ogawa.yui2000@gmail.com>
2023-08-28 21:40:58 +0900

    vue-router 404

---
+++ src/pages/errors/NotFoundErrorPage.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="container">
+        <h1>404</h1>
+        <p>Sorry, the page <code>{{ $route.params.notFound }}</code> you're looking for is not found</p>
+        <router-link :to="{ name: 'recipes' }" class="text-blue-600">Back to your recipes</router-link>
+    </div>
+</template>

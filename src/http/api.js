473bd07731fb860eebce63c6102e8c88f762fbc5
LB16B <ogawa.yui2000@gmail.com>
2023-09-03 17:58:10 +0900

    LoginAPI /api/auth/login

--- src/http/api.js
+++ src/http/api.js
@@ -1,14 +1,35 @@
+// import axios from "axios";
+// import cookies from "js-cookie";
+
+// axios.defaults.withCredentials = true;
+
+// const authToken = cookies.get('auth_token');
+
+// const headers = {
+//   Accept: 'application/json',
+//   Authorization: authToken ? `Bearer ${authToken}` : '',
+// };
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_BASE_URL,
+//   headers,
+// });
+
+
+// export default api;
+
 import axios from "axios";
 import cookies from "js-cookie";
+
 axios.defaults.withCredentials = true;
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     Accept: 'application/json',
     Authorization: cookies.isKey('auth_token') ? `Bearer ${cookies.get('auth_token')}` : '',
   },
 });
 
-
 export default api;
+
--- src/http/auth-api.js
+++ src/http/auth-api.js
@@ -1,12 +1,10 @@
 import api from "./api";
 
-
-
 export const csrfCookie = () => api.get("/sanctum/csrf-cookie");
 
 export const login = (credentials) => api.post("/api/auth/login", credentials);
 
-export const register = (user) => api.post("/auth/register", user);
+export const register = (user) => api.post("/api//auth/register", user);
 
 export const logout = () => api.post("/api/auth/logout");
 

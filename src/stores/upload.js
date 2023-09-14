156dae751b20850cb5094a2e83d7b12042a91cab
LB16B <ogawa.yui2000@gmail.com>
2023-09-14 18:32:28 +0900

    image trimming

--- src/stores/upload.js
+++ src/stores/upload.js
@@ -1,35 +1,21 @@
 import { defineStore } from "pinia";
 import { imageUpload } from "../http/upload-api";
 
-// ストアを定義
+
 export const useUploadStore = defineStore("uploadStore", () => {
-  // 画像のアップロード関数
-  const uploadRecipeImage = async (file) => {
-    try {
-      const formData = new FormData();
-      formData.append("file", file);
 
-      const response = await imageUpload(formData);
+    const uploadRecipeImage = async (formData) => {
+        try {
+            const response = await imageUpload(formData);
 
-      if (!response.ok) {
-        // サーバーからエラーレスポンスが返ってきた場合のエラーハンドリング
-        const errorData = await response.json();
-        throw new Error(errorData.message);
-      }
-
-      const data = await response.json();
-      console.log("アップロード成功:", data);
-
-      // 成功時の処理（必要に応じて追加）
+            const data = await response.data;
+            console.log("アップロード成功:", data);
 
     } catch (error) {
-      // エラーメッセージをコンソールに表示し、エラーを再スロー
-      // console.error("アップロードエラー:", error);
+      console.error("アップロードエラー:", error);
       throw error;
     }
   };
-
-  // ストアのエクスポート
   return {
     uploadRecipeImage,
   };

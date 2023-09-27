982756d26958133540ba11a6f2f1f733348f15c8
LB16B <ogawa.yui2000@gmail.com>
2023-09-27 22:16:07 +0900

    edit food

--- src/components/foods/EditFoods.vue
+++ src/components/foods/EditFoods.vue
@@ -7,8 +7,7 @@
             <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">必要な材料と容量</h1>
         </div>
 
-        <div 
-            class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+        <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
 
             <div class="p-2 sm:w-1/2 w-full">
                 <div class="bg-gray-100 rounded flex p-4 h-full items-center">
@@ -37,9 +36,38 @@
                 </div>
             </div>
         </div>
+        <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+
+            <div class="p-2 sm:w-1/2 w-full">
+                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-yellow-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                        <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                        <path d="M22 4L12 14.01l-3-3"></path>
+                    </svg>
+                    <input 
+                        type="text" 
+                        placeholder="にんじん"
+                        v-model="secondFoodIngredient"
+                        class="title-font font-medium bg-gray-100">
+                </div>
+            </div>
+            <div class="p-2 sm:w-1/2 w-full">
+                <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-yellow-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                    <path d="M22 4L12 14.01l-3-3"></path>
+                </svg>
+                <input 
+                    type="text" 
+                    placeholder="大さじ1"
+                    v-model="secondFoodAmount"
+                    class="title-font font-medium bg-gray-100">
+                </div>
+            </div>
+        </div>
         
         <button 
-            @click="UpdateFood"
+            @click="updateFoods"
             class="flex mx-auto mt-16 text-white bg-yellow-500 border-0 py-2 px-8 focus:outline-none hover:bg-yellow-600 rounded text-lg">
             投稿する
         </button>
@@ -65,7 +93,10 @@ const firstFoodId = ref('');
 const firstFoodIngredient = ref('');
 const firstFoodAmount = ref('');
 
-// props.foodsが変更された場合にfirstFoodIngredientとfirstFoodAmountを更新
+const secondFoodId = ref(''); 
+const secondFoodIngredient = ref('');
+const secondFoodAmount = ref('');
+
 watch(() => props.foods, (newFoods) => {
   if (newFoods.length > 0) {
     firstFoodId.value = newFoods[0].id;
@@ -75,20 +106,40 @@ watch(() => props.foods, (newFoods) => {
     firstFoodIngredient.value = '';
     firstFoodAmount.value = '';
   }
+
+  if (newFoods.length > 1) {
+    secondFoodId.value = newFoods[1].id;
+    secondFoodIngredient.value = newFoods[1].ingredient;
+    secondFoodAmount.value = newFoods[1].amount;
+  } else {
+    secondFoodIngredient.value = '';
+    secondFoodAmount.value = '';
+  }
 });
 
 
-const UpdateFood = async (event) => {
 
-const food = {
+const updateFoods = async (event) => {
+  const firstFood = {
     id: firstFoodId.value,
     ingredient: firstFoodIngredient.value,
     amount: firstFoodAmount.value,
-};
-console.log(food);
+  };
 
-handleUpdatedFood(food);
+  const secondFood = {
+    id: secondFoodId.value,
+    ingredient: secondFoodIngredient.value,
+    amount: secondFoodAmount.value,
+  };
 
-emit("updated", food);
+  console.log("一つ目の食品:", firstFood);
+  console.log("ふたつ目の食品:", secondFood);
+
+  // ここで一つ目とふたつ目の食品を更新する
+  handleUpdatedFood(firstFood);
+  handleUpdatedFood(secondFood);
+
+  // 更新を通知
+  emit("updated", [firstFood, secondFood]);
 };
 </script>

1a224565fd33fb1d59bb1451c8c4cd70ae1a7b36
LB16B <ogawa.yui2000@gmail.com>
2023-10-14 23:01:00 +0900

    category food

--- src/pages/recipes/SearchRecipePage.vue
+++ src/pages/recipes/SearchRecipePage.vue
@@ -1,7 +1,31 @@
 <template>
-
+    <main>
+        <Recipes :recipes="filterRecipe" />
+    </main>
 </template>
 
 <script setup>
+import { onMounted, ref, computed, watch } from "vue";
+import { storeToRefs } from "pinia"
+import { useRecipeStore } from "../../stores/recipe";
+import { useRoute, useRouter } from 'vue-router';
+import Recipes from '../../components/recipes/Recipes.vue';
+import api from "../../http/api";
+
+const store = useRecipeStore()
+const { recipes } = storeToRefs(store)
+const { fetchAllRecipes } = store
+
+onMounted(async () => {
+    await fetchAllRecipes()
+});
+
+const route = useRoute()
+let urlParameterSearchWord = null;
+
+const filterRecipe = computed(() => {
+    return recipes.value.filter(recipe => recipe.title === route.params.searchWord);
+})
+
 
 </script>

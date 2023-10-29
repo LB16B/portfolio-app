49edb9ffe37ac6d181a2ce7417ad0ce62393318b
LB16B <ogawa.yui2000@gmail.com>
2023-10-29 17:39:04 +0900

    refactoring

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,24 +1,42 @@
 <template>
 
-    <div class="text-gray-600 body-font bg-red-100 ">
+    <div class="text-gray-600 body-font bg-red-100 reactive h-36">
 
       
-      <div class="flex justify-center pt-4 "  v-if="store.isLoggedIn">
-        <a href="http://localhost:5173/top" class="flex title-font font-medium items-center2 text-gray-900 md:mb-0 mr-10">
+      <div class="flex justify-center py-4 mx-auto  w-2/3"  v-if="store.isLoggedIn">
+        <a href="http://localhost:5173/top" class="flex title-font font-medium items-center2 text-gray-900 mr-10 ">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-pink-500 rounded-full" viewBox="0 0 24 24">
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
           <span class="ml-3 text-xl">Tailblocks</span>
         </a>
-        <RecipeSearch  />
+        <RecipeSearch class=""  />
+
+        <div class="ml-12 flex items-center  relative w-1/12 h-14">
+          <img 
+          @click="pullDown"
+          class="rounded-full h-14 w-14 absolute bottom-2"
+          v-if="store.user && store.user.filename" :src="'http://localhost:8000/profile_images/' + store.user.filename"
+          />
+    
+          <div class="absolute top-12 w-40 right-2">
+            <div v-if="isPullDownVisible" class="bg-red-50 rounded mt-2 p-2 ">
+              <a href="#" @click.prevent="logout"
+                class="flex items-center justify-center">
+                Logout
+              </a>
+            </div>
+          </div>
+        </div>
+
       </div>
-        <div class="container mx-auto flex flex-wrap p-2 mt-2 flex-col md:flex-row items-center">
+        <div class="md:mr-auto  flex flex-wrap  mx-auto justify-center text-xl pt-8 relative w-3/5">
           <nav 
             v-if="store.isLoggedIn"
-            class="md:mr-auto md:ml-4 md:py-1 md:pl-4 flex flex-wrap items-center justify-center text-xl">
+            class="absolute bottom-0">
             <router-link 
               :to="{ name: 'new_recipe' }"
-              class="mr-5 hover:text-gray-900"
+              class="mr-5 hover:text-gray-900 "
               >
               レシピを投稿する
             </router-link>
@@ -43,24 +61,20 @@
             </router-link>
 
           </nav>
-          <img 
-
-            class="rounded-full h-16 w-16"
+          <!-- <img 
+            @click="pullDown"
+            class="rounded-full h-16 w-16 absolute right-8 bottom-2"
             v-if="store.user && store.user.filename" :src="'http://localhost:8000/profile_images/' + store.user.filename"
           />
 
-          <div class="container mx-auto flex flex-wrap p-2 mt-2 flex-col md:flex-row items-center">
-            <!-- プルダウンメニューボタン -->
-            <button @click="pullDown" class="text-xl inline-flex items-center bg-pink-500 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded mt-4 md:mt-0">
-              プルダウンメニュー
-            </button>
-            <!-- プルダウンメニューの内容 -->
-            <div v-if="isPullDownVisible" class="bg-white border rounded mt-2 p-2 absolute right-0">
-              <!-- メニューアイテムをここに追加 -->
-                <div>www</div>
-              <!-- 必要なメニューアイテムを追加... -->
+          <div class="container mx-auto flex flex-wrap p-2 mt-2 flex-col md:flex-row items-center relative">
+            <div v-if="isPullDownVisible" class="bg-red-50 rounded mt-2 p-2 absolute top-4 right-0">
+              <a href="#" @click.prevent="logout"
+                class=" inline-flex items-center border-t-2 border-b-2 border-gray-500 py-1 px-3 focus:outline-none  hover:bg-gray-200  mt-4 md:mt-0 text-xl">
+                Logout
+              </a>
             </div>
-          </div>
+          </div> -->
 
 
           <template v-if="!store.isLoggedIn">
--- src/components/RecipeSearch.vue
+++ src/components/RecipeSearch.vue
@@ -1,12 +1,12 @@
 <template>
 
-<div class="flex w-5/12 ">
-    <div class="relative flex-grow w-full mr-4">
-        <input v-model="searchQuery" placeholder="レシピを検索" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+<div class="flex w-3/6 relative h-3/4">
+    <div class="flex-grow">
+        <input v-model="searchQuery" placeholder="レシピを検索" class=" bg-gray-100 w-5/6 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
     </div>
     <button 
         @click="searchRecipes"
-        class="text-white bg-pink-500 border-0 py-2 w-24 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+        class="absolute right-0 text-white bg-pink-500 border-0 py-2 w-24 focus:outline-none hover:bg-pink-600 rounded text-lg">
         検索する
     </button>
 </div>

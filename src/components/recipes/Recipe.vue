d14568c1e1c3535a65e86d80e57eec4931aab46b
LB16B <ogawa.yui2000@gmail.com>
2023-11-02 17:38:17 +0900

    refactoring

--- src/components/recipes/Recipe.vue
+++ src/components/recipes/Recipe.vue
@@ -9,20 +9,37 @@
                 >
             </a>
             <div class="mt-4">
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">食材{{ recipe.category_food_id }}</h3>
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">月齢{{ recipe.category_age_id }}</h3>
-                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{{ recipe.title }}</h3>
+                <span>{{ categoryFood }}</span>
                 <h2 class="text-gray-900 title-font text-lg font-medium">{{ recipe.title }}</h2>
-                <span class="mt-1 mr-3">{{ recipe.time }}分</span><span class="mt-1">{{ recipe.price }}円</span>
+                <span class="mt-1 mr-3">約{{ recipe.time }}分</span><span class="mt-1">/約{{ recipe.price }}円</span>
             </div>
         </router-link>
     </div>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
+import { storeToRefs } from "pinia"
 import { useRecipeStore } from "../../stores/recipe";
 import RecipeActions from './RecipeActions.vue';
+import { useRoute, useRouter } from 'vue-router';
+import { useCategoryFoodStore } from "../../stores/category-food";
+import CategoryFoods from "../../components/categoryFoods/CategoryFoods.vue";
+import api from "../../http/api";
+
+const route = useRoute();
+const categoryFoodStore = useCategoryFoodStore()
+const { categoryFoods } = storeToRefs(categoryFoodStore)
+const { fetchAllCategoryFoods } = categoryFoodStore
+const categoryFood = ref('')
+
+onMounted(async () => {
+    await fetchAllCategoryFoods()
+    categoryFood.value = categoryFoods.value[route.params.foodId -1].name
+});
+
+
+
 
 const path = "http://localhost:8000/recipe_images/";
 
@@ -34,6 +51,7 @@ const props = defineProps({
     recipe: Object,
     selectedFile: Object 
 })
+
 
 </script>
 

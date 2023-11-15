5e8fdfb6f5f1b339f52af126cf984c1f7fac430c
LB16B <ogawa.yui2000@gmail.com>
2023-11-15 21:09:44 +0900

    refactoring

--- src/components/reviews/Review.vue
+++ src/components/reviews/Review.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="p-2  md:w-1/2 w-1/3 ">
-        <div class="h-full flex items-center border-gray-200 border p-4 rounded-lg ">
+        <div class="h-full flex items-center bg-orange-50  p-4 rounded-lg ">
             <img 
                 alt="team" 
                 class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" 
@@ -8,7 +8,7 @@
             >
             <div class="flex-grow">
                 <h2 class="text-gray-900 title-font font-medium">{{ matchingUser.name }}</h2>
-                <p class="text-gray-500">{{ review.score }}</p>
+                <p class="text-gray-500">{{ displayStars(review.score) }}</p>
                 <p class="text-gray-500">{{ review.body }}</p>
             </div>
         </div>
@@ -26,8 +26,6 @@ const props = defineProps({
     review: Object
 })
 
-// console.log(props.review.body)
-
 const router = useRouter()
 const route = useRoute();
 const userStore = useUserProfileStore()
@@ -38,13 +36,21 @@ const matchingUser = ref([])
 
 onMounted(async () => {
     await fetchAllUsers();
-    // user.value = userStore.users;
-    // console.log(user)
-
-      // props.review.user_id と一致するユーザーを見つける
-  matchingUser.value = userStore.users.find(u => u.id === props.review.user_id);
-    // console.log(matchingUser);
-
+    matchingUser.value = userStore.users.find(u => u.id === props.review.user_id);
 });
 
+
+const displayStars = (score) => {
+  if (props.review.score === 1) {
+    return '★☆☆☆☆';
+  } else if (props.review.score === 2) {
+    return '★★☆☆☆';
+  } else if (props.review.score === 3) {
+    return '★★★☆☆';
+  } else if (props.review.score === 4) {
+    return '★★★★☆';
+  } else if (props.review.score === 5) {
+    return '★★★★★';
+  }
+};
 </script>

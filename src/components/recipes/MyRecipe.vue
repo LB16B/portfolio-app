a81df3b6697bf61396ccac7baecfea015f1b6677
LB16B <ogawa.yui2000@gmail.com>
2023-12-11 21:39:43 +0900

    refactoring

--- src/components/recipes/MyRecipe.vue
+++ src/components/recipes/MyRecipe.vue
@@ -9,12 +9,20 @@
         </a>
         <div class="mt-4 w-1/2">
             <h2 class="text-gray-900 title-font text-xl font-medium">{{ recipe.title }}</h2>
-            <router-link :to="{ name: 'edit_recipe', params: { recipeId: recipe.id } }">
-                <div class="bg-pink-500 w-10 h-10 rounded-full flex items-center">
-                    <img src="../../assets//images/edit.png" class="mx-auto  w-6">
+            <div class="flex">
+                <router-link :to="{ name: 'edit_recipe', params: { recipeId: recipe.id } }">
+                    <div class="bg-pink-500 hover:bg-pink-600 w-10 h-10 rounded-full flex items-center">
+                        <img src="../../assets//images/edit.png" class="mx-auto  w-6">
+                    </div>
+                </router-link>
+                <div 
+                    @click="handleDelete"
+                    class="bg-pink-500 hover:bg-pink-600 w-10 h-10 rounded-full flex items-center ml-4"
+                >
+                    <img src="../../assets/images/delete.png" class="mx-auto  w-6">
                 </div>
-            </router-link>
             </div>
+        </div>
     </div>
     
     
@@ -24,6 +32,10 @@
 import { ref, computed } from "vue";
 import { useRecipeStore } from "../../stores/recipe";
 import RecipeActions from './RecipeActions.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const store = useRecipeStore();
 
 const path = "http://localhost:8000/recipe_images/";
 
@@ -35,6 +47,13 @@ const props = defineProps({
     recipe: Object,
     selectedFile: Object 
 })
+
+const handleDelete = () => {
+    store.handleRemovedRecipe(props.recipe);
+    // オプション：削除後に特定のページにリダイレクトする場合
+    // router.push({ name: 'desired_route_name' });
+    console.log('削除成功')
+};
 
 </script>
 
